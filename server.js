fe16be834376308414a631c345f36b7ae1957401
PrajwalK44 <153058777+PrajwalK44@users.noreply.github.com>
2024-11-29 16:51:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // Routes
-app.use('/schools', schoolRoutes);
-
+app.use("/api/schools", schoolRoutes); // Prefix routes with `/api`
 // Start server
 const PORT = process.env.PORT || 3000;
 module.exports = app; 
